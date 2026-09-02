70469affce69a4f1f716930cdd3d7d92edd0fb87
dhruvbharija8-lang <dhruvbharija8@gmail.com>
2026-09-02 09:02:00 +0530

    Remove Google sign-in button from LoginPage
    Removed Google sign-in button and login divider from the LoginPage component.

--- react-app/src/pages/LoginPage.tsx
+++ react-app/src/pages/LoginPage.tsx
@@ -181,20 +181,6 @@ export default function LoginPage() {
           </button>
           <a href="#" className="forgot-link" onClick={(e) => { e.preventDefault(); openForgotModal(); }}>Forgot password?</a>
 
-          <div className="login-divider"><span>or</span></div>
-
-          <button type="button" className="ps-btn ps-btn-ghost login-secondary-btn" onClick={() => alert('Google sign-in clicked')}>
-            <svg className="google-icon" viewBox="0 0 48 48" width={18} height={18}>
-              <path fill="#FFC107" d="M43.6 20.5H42V20H24v8h11.3c-1.6 4.7-6.1 8-11.3 8-6.6 0-12-5.4-12-12s5.4-12 12-12c3.1 0 5.9 1.2 8 3.1l5.7-5.7C34.5 6.1 29.5 4 24 4 12.9 4 4 12.9 4 24s8.9 20 20 20 20-8.9 20-20c0-1.3-.1-2.7-.4-3.5z" />
-              <path fill="#FF3D00" d="M6.3 14.7l6.6 4.8C14.5 15.1 18.9 12 24 12c3.1 0 5.9 1.2 8 3.1l5.7-5.7C34.5 6.1 29.5 4 24 4 16.3 4 9.7 8.3 6.3 14.7z" />
-              <path fill="#4CAF50" d="M24 44c5.4 0 10.3-2.1 14-5.5l-6.5-5.3C29.5 34.9 26.9 36 24 36c-5.2 0-9.6-3.3-11.3-7.9l-6.5 5C9.6 39.6 16.3 44 24 44z" />
-              <path fill="#1976D2" d="M43.6 20.5H42V20H24v8h11.3c-.8 2.3-2.3 4.3-4.2 5.7l6.5 5.3C40.9 35.6 44 30.3 44 24c0-1.3-.1-2.7-.4-3.5z" />
-            </svg>
-            Continue with Google
-          </button>
-        </form>
-      </div>
-
       {fpStep !== 'closed' && (
         <div className="ps-modal-overlay open">
           <div className="ps-modal" role="dialog" aria-modal="true">
